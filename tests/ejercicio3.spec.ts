3e3e9910f1d4686a66adf1077a3f0a0caf2f1cc2
Felipe <alu0101315713@ull.edu.es>
2022-03-27 22:46:12 +0100

    pruebas de Clase de Mensaje

--- tests/ejercicio3.spec.ts
+++ tests/ejercicio3.spec.ts
@@ -1,23 +1,23 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Mensaje} from '../src/ejercicio-3/msj';
-import {Clave} from '../src/ejercicio-3/clave';
-import {Cifrado} from '../src/ejercicio-3/cifrado';
-let msj = new Mensaje('HOLAESTOESUNAPRUEBA');
-let clave = new Clave('CLAVE');
-let c = new Cifrado(msj, clave);
+/* import {Clave} from '../src/ejercicio-3/clave';
+import {Cifrado} from '../src/ejercicio-3/cifrado';*/
+const msj = new Mensaje('HOLAESTOESUNAPRUEBA');
+// let clave = new Clave('CLAVE');
+// let c = new Cifrado(msj, clave);
 describe('Clase Mensaje', () => {
   it('Mensaje a encriptar', () => {
-    return expect(msj.getMsj()).to.be.equal('HOLAESTOESUNAPRUEBA');
+    return expect(msj.getMensaje()).to.be.equal('HOLAESTOESUNAPRUEBA');
   });
   it('Tamaño del mensaje', () => {
-    return expect(msj.getNumeroCaracteres()).to.be.equal(19);
+    return expect(msj.getTamañoMensaje()).to.be.equal(19);
   });
   it('Cantidad de caracteres del mensaje', () => {
     return expect(msj.getCaracteres(3)).to.be.equal('A');
   });
 });
-describe('Clase Clave', () => {
+/* describe('Clase Clave', () => {
   it('Clave a encriptar', () => {
     return expect(clave.getClave()).to.be.equal('CLAVE');
   });
@@ -47,4 +47,4 @@ describe('Clase Cifrado César', () => {
   it('Mensaje desencriptado', () => {
     return expect(c.desencriptar(msj, clave)).to.be.equal('\u0000');
   });
-});
+});*/
